ec718ea4edf4876fffa628ede7fe0016c3598773
Preston Badeer <prrstn@gmail.com>
2011-12-20 16:11:24 -0600

    Cheat implementation 2
    Press 'c' for cheats!!

--- Game/cheat.js
+++ Game/cheat.js
@@ -1,24 +1,34 @@
+// Look for keydown of 'c'
 window.document.onkeydown = function(e)
 {
     input = e ? e.keyCode : event.keyCode;
     
-    if(input == 67 && Cheat.On == false)
+    if(input == 67 && !Cheat.On)
     {
+        // Activate cheats!
         Cheat.create();
-        alert('Cheat codes activated.');
     }
 }
 
 
+// Create cheat object
 var Cheat = {
     On: false
 };
 
 
+// Cheat activation
 Cheat.create = function()
 {
+    // Set variables
     this.On = true;
-    this.Invincible = true;
+    this.Invincible = false;
+
+    // Create and display cheat menu
+    var div = document.createElement('div');
+    div.setAttribute('style', 'position:absolute;bottom:0;left:50%;width:400px;margin-left:-200px;');
+    div.innerHTML = "<a href='javascript:Cheat.invincible()'>invincible</a> <a href='javascript:Cheat.addCookies()'>cookies</a> <a href='javascript:Cheat.lowGravity()'>low-gravity</a> <a href='javascript:Cheat.noClipping()'>no-clip</a> <a href='javascript:Cheat.fastBaddies()'>fast-baddies</a> <a href='javascript:Cheat.fastBugsby()'>fast-bugsby</a>";
+    document.body.appendChild(div);
 
     Cheat.invincible = function()
     {
@@ -28,7 +38,7 @@ Cheat.create = function()
             this.Invincible = true;
     }
 
-    Cheat.cookie = function()
+    Cheat.addCookies = function()
     {
         NumCookies = NumCookies+100;
     }
@@ -38,7 +48,7 @@ Cheat.create = function()
         Gravity = 1;
     }
 
-    Cheat.noClip = function()
+    Cheat.noClipping = function()
     {
         Wall.prototype.WallDetect = function(bugsby)
         {
